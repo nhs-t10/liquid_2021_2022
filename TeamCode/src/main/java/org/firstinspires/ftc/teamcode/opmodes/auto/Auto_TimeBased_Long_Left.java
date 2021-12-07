146b3079c05d9aadca89240c51d7dbdfc52e0b1e
arlanz <arlan.zbarsky@gmail.com>
2021-12-07 18:39:36 -0500

    Added some timebased auto files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Auto_TimeBased_Long_Left.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.managers.FeatureManager;
+import org.firstinspires.ftc.teamcode.managers.manipulation.ManipulationManager;
+import org.firstinspires.ftc.teamcode.managers.movement.MovementManager;
+import org.firstinspires.ftc.teamcode.managers.telemetry.TelemetryManager;
+
+
+@Autonomous
+public class Auto_TimeBased_Long_Left extends OpMode {
+    private MovementManager driver;
+    private ManipulationManager hands;
+    float [] omniValues = new float [4];
+    int step = 1;
+    ElapsedTime timer;
+    public void delay(double delay) {
+        double endTime = timer.milliseconds() + delay;
+        while (timer.milliseconds() <= endTime) {
+            //do nothing
+        }
+
+
+    }
+
+    public void init() {
+        FeatureManager.setIsOpModeRunning(true);
+        DcMotor fl = hardwareMap.get(DcMotor.class, "fl");
+        DcMotor fr = hardwareMap.get(DcMotor.class, "fr");
+        DcMotor br = hardwareMap.get(DcMotor.class, "br");
+        DcMotor bl = hardwareMap.get(DcMotor.class, "bl");
+        DcMotor dw = hardwareMap.get(DcMotor.class, "dw");
+        hands = new ManipulationManager(
+                new CRServo[] {},
+                new String[] {},
+                new Servo[] {},
+                new String[] {},
+                new DcMotor[] {fl, fr, br, bl, dw},
+                new String[] {"fl", "fr", "br", "bl", "dw"}
+            );
+        driver = new MovementManager(fl, fr, br, bl);
+        telemetry = new TelemetryManager(telemetry, this, TelemetryManager.BITMASKS.NONE);
+        driver.setDirection();
+        timer = new ElapsedTime();
+
+    }
+    public void loop() {
+        switch (step) {
+            case(1):
+
+                driver.timeDriveOmni(4000, 0, 0, 0);
+                /*hands.setMotorPower("dw", 0.5);
+                delay(5000);
+                hands.setMotorPower("dw",0); */ //todo uncomment duck wheel laater
+                driver.timeDriveOmni(-5000,0,0, 0);
+                step++;
+                break;
+            case(2):
+                telemetry.addLine("Autonomous Complete");
+                telemetry.addData("time", timer.milliseconds());
+                telemetry.addData("Step #", step);
+                telemetry.update();
+        }
+    }
+
+}
+
