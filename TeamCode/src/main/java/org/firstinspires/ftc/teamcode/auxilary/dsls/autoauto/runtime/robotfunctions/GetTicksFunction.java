8131140edf4a9e996e6fb69d6832eb93860a3eee
Leololo8 <shraybmanleo@gmail.com>
2021-12-07 20:06:23 -0500

    Fixed CorbinTeleop to make the servo work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/dsls/autoauto/runtime/robotfunctions/GetTicksFunction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/dsls/autoauto/runtime/robotfunctions/GetTicksFunction.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.auxilary.dsls.autoauto.runtime.robotfunct
 import org.firstinspires.ftc.teamcode.auxilary.dsls.autoauto.model.values.*;
 import org.firstinspires.ftc.teamcode.auxilary.dsls.autoauto.runtime.NativeRobotFunction;
 import org.firstinspires.ftc.teamcode.managers.FeatureManager;
-
+/*
 public class GetTicksFunction extends NativeRobotFunction {
     private org.firstinspires.ftc.teamcode.managers.movement.MovementManager manager;
 
@@ -13,6 +13,7 @@ public class GetTicksFunction extends NativeRobotFunction {
 
     @Override
     public AutoautoPrimitive call(AutoautoPrimitive[] args) {
-        if(args.length == 0) {return new AutoautoNumericValue(manager.getTicks());}throw new org.firstinspires.ftc.teamcode.auxilary.dsls.autoauto.runtime.errors.AutoautoNoNativeMethodOverloadException("No getTicks with 0 args");
+        if(args.length == 0) {return new AutoautoNumericValue}throw new org.firstinspires.ftc.teamcode.auxilary.dsls.autoauto.runtime.errors.AutoautoNoNativeMethodOverloadException("No getTicks with 0 args");
     }
 }
+ */
