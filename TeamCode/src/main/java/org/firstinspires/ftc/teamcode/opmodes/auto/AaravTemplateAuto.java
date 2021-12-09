bc0d4b6c8333462cf289a782545768f12d74552c
avb251 <avb251@students.needham.k12.ma.us>
2021-12-09 15:26:24 -0500

    Created duck wheel long with time based.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AaravTemplateAuto.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.managers.FeatureManager;
+import org.firstinspires.ftc.teamcode.managers.imu.ImuManager;
+import org.firstinspires.ftc.teamcode.managers.manipulation.ManipulationManager;
+import org.firstinspires.ftc.teamcode.managers.movement.MovementManager;
+import org.firstinspires.ftc.teamcode.managers.telemetry.TelemetryManager;
+
+
+@Autonomous
+public class AaravTemplateAuto extends OpMode {
+    private MovementManager driver;
+    private ManipulationManager hands;
+    private ImuManager gyro;
+    int step = 1;
+    public BNO055IMU imu;
+    ElapsedTime timer;
+    public void delay(double delay) {
+        double endTime = timer.milliseconds() + delay;
+        while (timer.milliseconds() <= endTime) {
+            //do nothing
+        }
+
+
+    }
+
+    public void init() {
+        FeatureManager.setIsOpModeRunning(true);
+        DcMotor fl = hardwareMap.get(DcMotor.class, "fl");
+        DcMotor fr = hardwareMap.get(DcMotor.class, "fr");
+        DcMotor br = hardwareMap.get(DcMotor.class, "br");
+        DcMotor bl = hardwareMap.get(DcMotor.class, "bl");
+        DcMotor dw = hardwareMap.get(DcMotor.class, "dw");
+        hands = new ManipulationManager(
+                new CRServo[] {},
+                new String[] {},
+                new Servo[] {},
+                new String[] {},
+                new DcMotor[] {fl, fr, br, bl, dw},
+                new String[] {"fl", "fr", "br", "bl", "dw"}
+            );
+        driver = new MovementManager(fl, fr, br, bl);
+        driver.runUsingEncoders();
+        telemetry = new TelemetryManager(telemetry, this, TelemetryManager.BITMASKS.NONE);
+        driver.setDirection();
+        timer = new ElapsedTime();
+        gyro = new ImuManager(imu);
+
+    }
+    public void loop() {
+        float wheelSpeed = 0.5f;
+        switch (step) {
+            case(1):
+                driver.timeDriveRaw(2500, wheelSpeed, wheelSpeed, wheelSpeed, wheelSpeed);
+                step++;
+                break;
+            case(2):
+                hands.setMotorPower("dw", -0.5);
+                step++;
+                break;
+            case(3):
+                driver.timeDriveRaw(3000, -wheelSpeed, -wheelSpeed, -wheelSpeed, -wheelSpeed);
+                step++;
+                break;
+        }
+        telemetry.addLine("Encoder Values");
+        telemetry.addData("fl pos", driver.flGetTicks());
+        telemetry.addData("fr pos", driver.frGetTicks());
+        telemetry.addData("bl pos", driver.blGetTicks());
+        telemetry.addData("br pos", driver.brGetTicks());
+        telemetry.update();
+    }
+
+}
+
