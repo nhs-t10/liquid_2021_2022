cc1a21ccd9d4db4f5f36f836e065dbe6063a5f94
Corbin <cfs251@students.needham.k12.ma.us>
2021-12-14 19:10:27 -0500

    test range sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RangeSensor.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static com.google.blocks.ftcrobotcontroller.hardware.HardwareType.BNO055IMU;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.teamcode.auxilary.PaulMath;
+import org.firstinspires.ftc.teamcode.managers.FeatureManager;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.managers.imu.ImuManager;
+import org.firstinspires.ftc.teamcode.managers.manipulation.ManipulationManager;
+import org.firstinspires.ftc.teamcode.managers.movement.MovementManager;
+import org.firstinspires.ftc.teamcode.managers.telemetry.TelemetryManager;
+
+
+@Autonomous
+public class RangeSensor extends OpMode {
+    private MovementManager driver;
+    private ManipulationManager hands;
+    ModernRoboticsI2cRangeSensor rangeSensor;
+    int step = 1;
+    ElapsedTime timer;
+
+    public void delay(double delay) {
+        double endTime = timer.milliseconds() + delay;
+        while (timer.milliseconds() <= endTime) { }
+    }
+
+    public void init() {
+        FeatureManager.setIsOpModeRunning(true);
+        DcMotor fl = hardwareMap.get(DcMotor.class, "fl");
+        DcMotor fr = hardwareMap.get(DcMotor.class, "fr");
+        DcMotor br = hardwareMap.get(DcMotor.class, "br");
+        DcMotor bl = hardwareMap.get(DcMotor.class, "bl");
+        DcMotor dw = hardwareMap.get(DcMotor.class, "dw");
+        hands = new ManipulationManager(
+                new CRServo[] {},
+                new String[] {},
+                new Servo[] {},
+                new String[] {},
+                new DcMotor[] {fl, fr, br, bl, dw},
+                new String[] {"fl", "fr", "br", "bl", "dw"}
+        );
+        driver = new MovementManager(fl, fr, br, bl);
+        driver.runUsingEncoders();
+        telemetry = new TelemetryManager(telemetry, this, TelemetryManager.BITMASKS.NONE);
+        driver.setDirection();
+        timer = new ElapsedTime();
+        rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range");
+    }
+    public void loop() {
+        switch (step) {
+            case(1):
+                step++;
+                break;
+            case(2):
+                step++;
+                break;
+            case(3):
+                telemetry.addLine("Autonomous Complete");
+                telemetry.addData("time", timer.milliseconds());
+                telemetry.addData("Step #", step);
+                telemetry.update();
+        }
+        telemetry.addLine("Encoder Values");
+        telemetry.addData("fl pos", driver.flGetTicks());
+        telemetry.addData("fr pos", driver.frGetTicks());
+        telemetry.addData("bl pos", driver.blGetTicks());
+        telemetry.addData("br pos", driver.brGetTicks());
+        telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic());
+        telemetry.addData("raw optical", rangeSensor.rawOptical());
+        telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
+        telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
+        telemetry.update();
+    }
+
+}
