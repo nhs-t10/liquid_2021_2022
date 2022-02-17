5ad98053815e2bfb43548c507e051048d1566d21
arlanz <arlan.zbarsky@gmail.com>
2022-02-17 14:46:25 -0500

    updated freight drop off

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RangeSensorBlueAutoFreightDropOff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RangeSensorBlueAutoFreightDropOff.java
@@ -117,7 +117,7 @@ public class RangeSensorBlueAutoFreightDropOff extends OpMode {
                 final double initDist1 = backDist1.getDistance(CM);
                 final double initDist2 = backDist2.getDistance(CM);
                 driver.driveRaw(-0.25f, -0.25f, -0.25f, -0.25f);
-                delayDriveStop(400);
+                delayDriveStop(4000);
                 break;
             case(2):
                 hands.setServoPower("isl", -1);
