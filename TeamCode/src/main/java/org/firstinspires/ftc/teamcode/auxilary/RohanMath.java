4f29681412b7b615002296f0a7a2cae074d223b2
Rohan <rohanCP26@gmail.com>
2022-03-15 19:33:01 -0400

    new function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/RohanMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auxilary/RohanMath.java
@@ -1,17 +1,32 @@
 package org.firstinspires.ftc.teamcode.auxilary;
 
 
+import static org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.CM;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+
 import org.firstinspires.ftc.teamcode.managers.FeatureManager;
 
 public abstract class RohanMath extends FeatureManager {
 
-    public static int convertDegrees(int degree) {
+    public static int convertDeg(int degree) {
         if (degree < 0 || degree > 360) {
             return -1;
         } else {
             int newDeg = degree - 180;
         }
         return degree;
+    }
+    public static double getDistanceTrig(double d1, double d2){
+        int theta = 90;
+        if(d1 > 300){
+            d1 = d2;
+        }
+        else if(d2 > 300){
+            d2 = d1;
+        }
+        double h = (((d2)/Math.pow(2.0, 0.5))+((d1)/Math.pow(2.0, 0.5)))/2;
+        return h;
     }
 
 }
